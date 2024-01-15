7279c941678f1ca1f12a7915e0f89b6c95b6d99e
Lennon Fonseca <lennonfonsecaa@gmail.com>
2024-01-15 13:42:09 -0300

    exerciseExplorer

---
+++ script.js
@@ -0,0 +1,108 @@
+//ADIÇÃO
+
+/* alert('Vamos fazer uma conta de soma? Siga os próximos passos!')
+
+let numberOne = prompt('Digite um número: ')
+let numberTwo = prompt('Digite outro número: ')
+
+function mathOperation() {
+    result = Number(numberOne) + Number(numberTwo)
+}
+
+mathOperation()
+alert(`o Resultado da soma é: ${result}`) */
+
+
+
+//SUBTRAÇÃO
+
+/* alert('Vamos fazer uma conta de subtração? Siga os próximos passos!')
+
+let numberOne = prompt('Digite um número: ')
+let numberTwo = prompt('Digite outro número: ')
+
+function mathOperation() {
+    result = Number(numberOne) - Number(numberTwo)
+}
+
+mathOperation()
+alert(`o Resultado da subtração é: ${result}`) */
+
+
+
+//MULTIPLICAÇÃO
+
+/* alert('Vamos fazer uma conta de multiplicação? Siga os próximos passos!')
+
+let numberOne = prompt('Digite um número: ')
+let numberTwo = prompt('Digite outro número: ')
+
+function mathOperation() {
+    result = Number(numberOne) * Number(numberTwo)
+}
+
+mathOperation()
+alert(`o Resultado da multiplicação é: ${result}`) */
+
+
+
+// DIVISÃO
+
+/* alert('Vamos fazer uma conta de divisão? Siga os próximos passos!')
+
+let numberOne = prompt('Digite um número: ')
+let numberTwo = prompt('Digite outro número: ')
+
+function mathOperation() {
+    result = Number(numberOne) / Number(numberTwo)
+}
+
+mathOperation()
+alert(`o Resultado da divisão é: ${result}`) */
+
+
+
+//RESTO DA DIVISÃO 
+
+/* alert('Vamos fazer uma conta para verificar o resto da divisão? Siga os próximos passos!')
+
+let numberOne = prompt('Digite um número: ')
+let numberTwo = prompt('Digite outro número: ')
+
+function mathOperation() {
+    result = Number(numberOne) % Number(numberTwo)
+}
+
+mathOperation()
+alert(`o Resto da divisão é: ${result}`) */
+
+
+
+//PAR OU ÍMPAR?
+
+/* alert('Par ou Ímpar? Some dois números e verifique o resultado!')
+
+let numberOne = prompt('Digite um número: ')
+let numberTwo = prompt('Digite outro número: ')
+
+let result = Number(numberOne) % Number(numberTwo)
+
+
+if (result == 0) {
+    alert('O número é par!')
+} else {
+    alert('O número é Ímpar!')
+} */
+
+alert('Digite dois números e verifique se são iguais... Siga os próximos passos!')
+
+let numberOne = prompt('Digite um número: ')
+let numberTwo = prompt('Digite outro número: ')
+
+let result = Number(numberOne) == Number(numberTwo)
+
+if (result == true) {
+    alert('Os números são iguais!')
+} else {
+    alert('os números são diferentes!')
+}
